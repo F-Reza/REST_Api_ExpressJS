739542d9916e066880dac73892380e4f2172ee74
Forhad Reza <forhadreza55@gmail.com>
2025-08-12 19:17:19 +0600

    add model

---
+++ server/models/user-model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true,
+    },
+    address: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+    
+        
