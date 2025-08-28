d4ddf7faf6c54f52dedcc42d5942075702cd2c0b
Forhad Reza <forhadreza55@gmail.com>
2025-08-28 11:54:11 +0600

    Add show data Contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useAuth } from '../store/auth';
 
 const Contact = () => {
+    const { user } = useAuth();
+    const userData = user?.data || {}; // Extract user data
+
+    console.log("Authenticated user in Contact page:", user);
     return (
         <div>
             <h1>Contact Us</h1>
+            <h4>Email: {userData.email}</h4>
+            <h4>Name: {userData.name}</h4>
             <p>If you have any questions or feedback, feel free to reach out to us at:</p>
             <a href="mailto:info@gmail.com"/> 
 
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,9 +1,13 @@
 
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
+  // const token = localStorage.getItem("token");
+  // const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(""); 
 
   const storetokenInLS = (token) => {
     localStorage.setItem("token", token);
@@ -15,8 +19,42 @@ export const AuthProvider = ({ children }) => {
 
   let isLoggedIn = !!localStorage.getItem("token"); 
 
+
+
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/profile", {
+        method: "GET",  
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${token}`, 
+          Authorization: `Bearer ${localStorage.getItem("token")}`, 
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("User data:", data);
+        setUser(data);
+        isLoggedIn = true; 
+      } else {
+        console.error("Failed to fetch user data");
+        isLoggedIn = false; 
+      }
+
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
+
+
+
   return (
-    <AuthContext.Provider value={{ storetokenInLS, removeTokenFromLS, isLoggedIn }}>
+    <AuthContext.Provider value={{ storetokenInLS, removeTokenFromLS, isLoggedIn, user}}>
       {children}
     </AuthContext.Provider>
   );
